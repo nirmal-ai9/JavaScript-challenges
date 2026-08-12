983017074598e7641702c4f7a51549673a3957fa
Nirmal <nirmalprajapati9978@gmail.com>
2026-08-12 09:44:40 +0530

    Enhance chokidar.js with console logging
    Added console logs for activation and file details.

--- chokidar.js
+++ chokidar.js
@@ -2,12 +2,16 @@ const chokidar = require('chokidar');
 const path = require('path');
 const fs = require('fs');
 
+console.log("chokidar activated");
+
 const watchedFolders = ['"sum of digits"/solutions'];
 const watcher = chokidar.watch(watchedFolders, { ignoreInitial: true });
 
 watcher.on('add', (filePath) => {
   const folder = path.dirname(filePath);
   const fileName = path.basename(filePath);
+  console.log(folder);
+  console.log(fileName);
   const configPath = path.join(path.dirname(folder), "config.json");
   const data = JSON.parse(fs.readFileSync(configPath,  "utf8"));
 
@@ -21,5 +25,5 @@ if (require.main === module) {
   require("../../index.js")(__filename);
 }`;
   
-  fs.writeFileSync(filePath, template);  
+  fs.writeFileSync(filePath, template, "utf8");  
 });
