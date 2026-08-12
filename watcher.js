d61da31732e1e114b7f76cd5fc42232f0d39be63
Nirmal <nirmalprajapati9978@gmail.com>
2026-08-12 19:38:05 +0530

    Fix path format in watchedFolders array

--- watcher.js
+++ watcher.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 console.log("Watcher activated...");
 
-const watchedFolders = ['"sum of digits"/solutions'];
+const watchedFolders = ['sum of digits/solutions'];
 const watcher = chokidar.watch(watchedFolders, { ignoreInitial: true });
 
 watcher.on('add', (filePath) => {
