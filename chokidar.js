5888916afd4a388a4f481b7acc1a26c24c81799b
Nirmal <nirmalprajapati9978@gmail.com>
2026-08-12 08:54:41 +0530

    Initialize chokidar file watcher for folders
    Set up file watcher for specified folders using chokidar.

--- chokidar.js
+++ chokidar.js
@@ -1,1 +1,12 @@
+const chokidar = require('chokidar');
+const path = require('path');
+const fs = require('fs');
+const watchedFolders = [
+  '"sum of digits"/solutions'
+];
+const watcher = chokidar.watch(watchedFolders, { ignoreInitial: true });
 
+watcher.on('add', (filePath) => {
+  const folder = path.dirname(filePath);
+  const fileName = path.basename(filePath);
+});
